f3ebcc974b1786369a4e4dc87a0c4250278c2d9a
fhemery <f.hemery@hemit.fr>
2023-02-10 14:04:49 +0100

    Add Kotlin seed

---
+++ kotlin/src/test/kotlin/HelloTest.kt
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class HelloTest {
+    @Test
+    fun `should work`() {
+        assertEquals(3, 1 + 2)
+    }
+
+    @Test
+    fun `should fail`() {
+        assertEquals("Hello world!", Hello().sayHello())
+    }
+}

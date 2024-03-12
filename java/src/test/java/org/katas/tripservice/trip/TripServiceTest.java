e0bc86b7bdd12d72b1fae4f4e5bc20ef71ca8874
fhemery <f.hemery@hemit.fr>
2024-03-12 15:30:47 +0100

    TripService first overload in Java and TS

--- java/src/test/java/org/katas/tripservice/trip/TripServiceTest.java
+++ java/src/test/java/org/katas/tripservice/trip/TripServiceTest.java
@@ -1,5 +1,25 @@
 package org.katas.tripservice.trip;
 
+import org.junit.jupiter.api.Test;
+import org.katas.tripservice.exception.UserNotLoggedInException;
+import org.katas.tripservice.user.User;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class TripServiceOverload extends TripService {
+    @Override
+    protected User getUser() {
+        return null;
+    }
+}
+
 public class TripServiceTest {
-	
+
+    @Test
+    public void should_ThrowException_WhenUserIsNotLogged() {
+        TripService tripService = new TripServiceOverload();
+
+        assertThrows(UserNotLoggedInException.class, () -> tripService.getTripsByUser(null));
+    }
+
 }
--- ts/src/06 - TripService/test/trip-service.spec.ts
+++ ts/src/06 - TripService/test/trip-service.spec.ts
@@ -1,7 +1,19 @@
 import "jest";
+import TripService from "../src/trip/TripService";
+import User from "../src/user/User";
+import UserNotLoggedInException from "../src/exception/UserNotLoggedInException";
+
+class TripServiceOverload extends TripService {
+    protected getUser() {
+        return null!;
+    }
+
+}
 
 describe("TripServiceShould", () => {
-    it("...", () => {
-        expect(4 + 4).toBe(8);
+    it('should throw exception when user is not logged', () => {
+        const tripService = new TripServiceOverload();
+
+        expect(() => tripService.getTripsByUser(new User())).toThrowError(new UserNotLoggedInException());
     });
 });
